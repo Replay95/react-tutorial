27c5e4996281f1dadeb05b9206b0d21185501c60
Replay95 <lia0528Ma@gmail.com>
2024-05-21 13:17:30 +0900

    Day2/lesson5まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
 import React from "react";
 import { List } from "./List";
 import { Form } from "./Form";
+import { LANGUAGES } from "./const/languages";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { description: "クリック前の表示", tab: `list` };
+    this.state = {
+      tab: "list",
+      langs: LANGUAGES,
+    };
   }
-
+  addLang(lang) {
+    this.setState({
+      langs: [...this.state.langs, lang],
+      tab: "list",
+    });
+  }
   render() {
-    const { tab } = this.state;
+    const { tab, langs } = this.state;
     return (
       <div>
         <header>
           <ul>
-            <li onClick={() => this.setState({ tab: `list` })}>リスト</li>
-            <li onClick={() => this.setState({ tab: `form` })}>フォーム</li>
+            <li onClick={() => this.setState({ tab: "list" })}>リスト</li>
+            <li onClick={() => this.setState({ tab: "form" })}>フォーム</li>
           </ul>
         </header>
-
-        {tab === `list` ? <List /> : <Form />}
+        <hr />
+        {tab === "list" ? (
+          <List langs={langs} />
+        ) : (
+          <Form onAddLang={(lang) => this.addLang(lang)} />
+        )}
       </div>
     );
   }
 }
-
 export default App;
